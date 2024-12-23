b038afe56c031ae5422f981b1cbfb36ceff6d55d
Edward Van Vlasselaer <info@edito.dev>
2024-12-23 16:14:38 +0100

    added 6 more images to carousel

--- components/AboutCarousel.vue
+++ components/AboutCarousel.vue
@@ -6,21 +6,16 @@
   >
     <div style="position: relative">
       <SplideTrack>
-        <SplideSlide>
-          <img src="/images/carousel/1.jpeg" alt="" class="img-1" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src="/images/carousel/2.jpeg" alt="" class="img-2" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src="/images/carousel/3.jpeg" alt="" class="img-3" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src="/images/carousel/4.jpeg" alt="" class="img-4" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src="/images/carousel/5.jpeg" alt="" class="img-5" />
-        </SplideSlide>
+        <SplideSlide><img src="/images/carousel/1.jpeg" alt="Vrouw gooit confetti in de lucht tijdens een feest, zittend op de vloer in een paarse broek." class="img-1"/></SplideSlide>
+        <SplideSlide><img src="/images/carousel/2.jpeg" alt="Kinderen knutselen onder een partytent met regenboogdecoratie en ballonnen." class="img-2"/></SplideSlide>
+        <SplideSlide><img src="/images/carousel/3.jpeg" alt="Vrouw en meisje buiten met kleurrijke stickers op hun gezichten." class="img-3"/></SplideSlide>
+        <SplideSlide><img src="/images/carousel/4.jpeg" alt="Vrouw toont roze verjaardagstaart in een tuin met zwembad." class="img-4"/></SplideSlide>
+        <SplideSlide><img src="/images/carousel/5.jpeg" alt="Lachende vrouw met glas roze wijn in een tuin." class="img-5"/></SplideSlide>
+        <SplideSlide><img src="/images/carousel/6.jpeg" alt="Vrouw en kind met ballon tijdens een binnenevenement." class="img-5"/></SplideSlide>
+        <SplideSlide><img src="/images/carousel/7.jpeg" alt="Vrouw met peuter bij paarse ballondecoratie en teddybeer." class="img-5"/></SplideSlide>
+        <SplideSlide><img src="/images/carousel/8.jpeg" alt="Kinderen spelen met bellen op een trampoline in de tuin." class="img-5"/></SplideSlide>
+        <SplideSlide><img src="/images/carousel/9.jpeg" alt="Lachende vrouw voor een roze achtergrond met een zeepbel op haar hoofd." class="img-5"/></SplideSlide>
+        <SplideSlide><img src="/images/carousel/10.jpeg" alt="Vrouw poseert naast een kleurrijke ballonnenboog bij een feest." class="img-5"/></SplideSlide>
       </SplideTrack>
     </div>
   </Splide>
@@ -29,9 +24,8 @@
 <script setup lang="ts">
 const options = {
   width: '100%',
-  type: 'slide',
-  rewind: true,
-  rewindByDrag: false,
+  type: 'loop',
+  drag: false,
   autoplay: true,
   speed: 500,
   interval: 4000,
@@ -43,7 +37,7 @@ const options = {
 <style scoped lang="scss">
 .splide__slide img {
   width: 100%;
-  height:35vh;
+  height: 35vh;
   object-fit: cover;
   display: block;
 
@@ -62,12 +56,15 @@ const options = {
 .img-2 {
   object-position: 50% 50%;
 }
+
 .img-3 {
   object-position: 50% calc(50% + 6rem);
 }
+
 .img-4 {
   object-position: 50% calc(50% + 9rem);
 }
+
 .img-5 {
   object-position: 50% calc(50% + 5rem);
 }
