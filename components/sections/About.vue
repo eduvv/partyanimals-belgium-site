8cd851804e498183475a646e5630cf09c1b40d43
Edward Van Vlasselaer <info@edito.dev>
2024-12-30 13:34:53 +0100

    fontsize

--- components/sections/About.vue
+++ components/sections/About.vue
@@ -7,9 +7,9 @@
     <div class="max-w-4xl mx-auto text-start">
       <div class="mx-4 sm:mx-12 my-2 sm:mt-10">
         <h2 class="font-monograss text-4xl text-primary my-5">{{ $t('about_title') }}</h2>
-        <p class="text-lg sm:text-xl text-primary font-poppins mb-4">{{ $t('about_content[0]') }}</p>
-        <p class="text-lg sm:text-xl text-primary font-poppins mb-4">{{ $t('about_content[1]') }}</p>
-        <p class="text-lg sm:text-xl text-primary font-poppins mb-4">{{ $t('about_content[2]') }}</p>
+        <p class="text-base sm:text-lg text-primary font-poppins mb-4">{{ $t('about_content[0]') }}</p>
+        <p class="text-base sm:text-lg text-primary font-poppins mb-4">{{ $t('about_content[1]') }}</p>
+        <p class="text-base sm:text-lg text-primary font-poppins mb-4">{{ $t('about_content[2]') }}</p>
       </div>
       <AboutCarousel class="justify-self-center mx-0 sm:mx-12"/>
     </div>
